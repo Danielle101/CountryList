5c8a728daf6ba9c7e3f659a426624c016ab312fe
DanielleHall <halldan4@yahoo.com>
2016-04-24 23:57:11 -0400

    added and updated comments

--- src/lab15/CountriesApp.java
+++ src/lab15/CountriesApp.java
@@ -1,4 +1,5 @@
 package lab15;
+//This app allows you to create,read, and write to a text file
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,11 +9,13 @@ public class CountriesApp {
 		CountriesTextFile textFile = new CountriesTextFile();
 		Scanner scan = new Scanner(System.in);
 		int choice = 0;
+		// welcome message and  menu prompt for user
 		System.out.println("Welcome to the Countries Maintenance Application!\n");
 		System.out.println("1 - See the list of countries\n2 - Add a country\n3 - Exit\n");
 	
 		while(true)
 		{
+			//ask for user entry and validates that entry is withing range
 		choice = Validator.getIntWithinRange("Please select a number from the menu (1/2/3):", scan, 1, 3);
 		switch(choice)
 		{
@@ -26,6 +29,7 @@ public class CountriesApp {
 			textFile.writeCountries("Enter the country you would like to add:");
 			textFile.closeWrite();
 			break;
+			//exits the program
 		case 3:
 			System.out.println("Buh-bye");
 			System.exit(0);;
